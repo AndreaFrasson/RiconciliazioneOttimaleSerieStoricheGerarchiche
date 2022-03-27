f7413f78d407868b0ac36682fc63b054eb98271a
AndreaFrasson <frass99and@gmail.com>
2022-03-27 14:36:50 +0200

    Update AustralianScript.r

--- AustralianScript.r
+++ AustralianScript.r
@@ -10,11 +10,11 @@ library(splitTools)
 library(drat)
 library(xgboost)
 library(randomForest)
-source("C:/Users/and99/Desktop/Utility.r")
 
-
-#v4 
-AT <- read.csv("D:/Documenti/Tesina/TourismData_v4.csv")[2:79]
+#V3 con pourpose of travel
+#AT <- read.csv("D:/Documenti/Tesina/TourismData_v3.csv")
+#v4 del prof
+AT <- read.csv("C:/Users/Andrea Frasson/Desktop/Tesina/TourismData_v4.csv")[2:79]
 colnames(AT)[1:2] <- c("Year", "Month")
 #-----------------------------------------------------
 #Esplorazione dei dati
@@ -35,6 +35,7 @@ AT %>%
   mutate(Index = yearmonth(Index)) %>%
   as_tsibble(index = Index) -> AT_ts
 
+#####################
 #Fare alcune considerazioni circa la struttura dei dati, come sono divisi, i motivi del
 #viaggio e le regioni australiane ecc e poi plottare
 
@@ -71,6 +72,10 @@ autoplot(Lev0, value) +
   labs(title = "Total flow Australian tourism",
        y = "Passengers", x = "Year")
 
+#view(tourism)
+#dim(tourism)
+#dim(AT)
+
 #############################################################################
 #rolling-origin evaluation approach
 OnlyData <- AT[,3:78]
@@ -94,7 +99,6 @@ Region = c(BottomNames %>%
 
 C <- Cmatrix( ~ State/Zones/Region, name_bts, sep = "")
 
-
 #DIVISIONE DEL DATASET IN UNA PARTE DI TRAIN E UNA PARTE DI TEST
 #CREAZIONE DELLE GERARCHIE PER EFFETTUARE LE PREVISIONI
 set.seed(5)
@@ -115,32 +119,48 @@ hts_test <- cbind(test_uts, test_bts)
 #PREVISIONI BASE PER IL SET DI TRAIN CON ORIZZONTE DI PREVISIONE h = 1
 fitted <- lapply(1:ncol(hts_train), function(i) auto.arima(hts_train[,i]))
 basef <- lapply(1:ncol(hts_train), function(i) forecast(hts_train[,i], h = 1, model = fitted[[i]], level = 0.95)$mean)
+lapply(1:ncol(hts_train), function(i) forecast(hts_train[,i], h = 1, model = fitted[[i]], level = 0.95)$residuals) %>%
+  unlist() %>%
+  matrix(ncol = 105, ) -> Mres
 
 #RICONCILIAZIONE BOTTOM-UP
-basef[1:105] %>%
+basef %>%
   as.double() %>%
   t()%>%
   htsrec(., comb = "bu", C = C) -> cs_bu
 
 #MASE PER IL TOTALE
-getMASE(cs_bu$recf, hts_test[1,], hts_train ,dim(hts_train)[1], 1)
+Metrics::mase(actual = hts_test[1,], predicted = cs_bu$recf, step_size = 1)
 
 
 ### RICONCILIAZIONE TOP-DOWN
-# monthly base forecasts
-id <- which(simplify2array(strsplit(colnames(FoReco_data$base), split = "_"))[1, ] == "k1")
-mbase <- t(FoReco_data$base[, id])
-obs_1 <- FoReco_data$obs$k1
 # average historical proportions
-props <- colMeans(obs_1[1:168,-c(1:3)]/obs_1[1:168,1])
-cs_td <- tdrec(topf = mbase[,1], C = C, weights = props)
+props <- colMeans(hts_train[1:216,-c(1:29)]/hts_train[1:216,1])
+basef[1] %>%
+  as.double() %>%
+  t()%>%
+  tdrec(., C = C, weights = props) -> cs_td
+
+Metrics::mase(actual = hts_test[1,], predicted = cs_td, step_size = 1)
 
 
+#MINT SAMPLE
+basef %>%
+  as.double() %>%
+  t() %>%
+  htsrec(., comb = "sam", C = C, res = Mres) -> cs_minsam
 
+Metrics::mase(actual = hts_test[1,], predicted = cs_minsam$recf, step_size = 1)
 
+#MINT SHRINK
+basef %>%
+  as.double() %>%
+  t() %>%
+  htsrec(., comb = "shr", C = C, res = Mres) -> cs_minshr
 
+Metrics::mase(actual = hts_test[1,], predicted = cs_minshr$recf, step_size = 1)
 
-
+#ML RANDOM FOREST
 
 
 
